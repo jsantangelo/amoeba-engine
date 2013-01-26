6d1e0ff7e07fa28ccb718a8541f0fe7845fd46a0
jsantangelo <jamesfsantangelo@gmail.com>
2013-01-26 14:55:34 -0500

    [#15] Added basic test game class to demonstrate use of current AmoebaEngine implementation.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/examples/TestGame.java
@@ -0,0 +1,30 @@
+package com.pixelweaverstudios.amoeba.examples;
+
+import android.os.Bundle;
+
+import com.pixelweaverstudios.amoeba.activity.GameActivity;
+import com.pixelweaverstudios.amoeba.engine.IAmoebaEngine;
+
+/**
+ * A basic game written to prove, through use, the interface of the AmoebaEngine.
+ * @see AmoebaEngine
+ */
+public class TestGame extends GameActivity
+{
+	/**
+	 * Is invoked when the Android OS system creates this Activity. onCreate
+	 * is the entry point for all Android applications.
+	 *
+	 * @param savedInstanceState object that holds the instance state of an
+	 *  application for later recreation if necessary
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		IAmoebaEngine myEngine = getEngine();
+		//Screen mainMenu = createMainMenuScreen();
+		//myEngine.addScreen(mainMenu);
+
+		super.onCreate(savedInstanceState);
+	}
+}
