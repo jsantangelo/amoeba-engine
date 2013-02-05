9142ba098c8a8b05b52d754f6d7a6280a46a714d
jsantangelo <jamesfsantangelo@gmail.com>
2013-02-05 17:34:37 -0500

    [#26] Added some base classes to support gesture handling.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/IGestureConfigurator.java
@@ -0,0 +1,7 @@
+package com.pixelweaverstudios.amoeba.engine;
+
+public interface GestureConfigurator
+{
+	public void enableGesture(GestureType type);
+	public void disableGesture(GestureType type);
+}
