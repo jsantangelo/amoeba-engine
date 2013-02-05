e8a0860d640908101ed0083426907bc7ecaff97d
Mike Testen <mrtesten@gmail.com>
2013-02-04 19:39:59 -0500

    [#27] - Moved texture load functionality to TextureUtilities.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/utilities/TextureUtilities.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/utilities/TextureUtilities.java
@@ -1,6 +1,10 @@
 package com.pixelweaverstudios.amoeba.graphics.utilities;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
+import android.opengl.GLUtils;
 
 /**
  * @author Mike Testen
@@ -8,28 +12,61 @@ import android.opengl.GLES20;
  */
 public class TextureUtilities
 {
-    /**
-     * @param handle
-     * @return Whether the texture is loaded.
-     */
-    static public boolean isTextureLoaded(int handle)
-    {
-        return GLES20.glIsTexture(handle); 
-    }
-    
-    /**
-     * @return A new handle to be used in the binding of a texture.
-     */
-    static public int generateTextureHandle()
-    {
-        int[] temp = new int[1];
+	/**
+	 * @param handle
+	 * @return Whether the texture is loaded.
+	 */
+	public static boolean isTextureLoaded(int handle)
+	{
+		return GLES20.glIsTexture(handle);
+	}
 
-        GLES20.glGenTextures(1, temp, 0);
-        if(temp[0] == 0)
-        {
-            throw new RuntimeException("Unable to generate a new texture id.");
-        }
+	/**
+	 * @return A new handle to be used in the binding of a texture.
+	 */
+	public static int generateTextureHandle()
+	{
+		int[] temp = new int[1];
 
-        return temp[0];
-    }
+		GLES20.glGenTextures(1, temp, 0);
+		if(temp[0] == 0)
+		{
+			throw new RuntimeException("Unable to generate a new texture id.");
+		}
+
+		return temp[0];
+	}
+
+	public static void loadTextureFromResource(Context context, Texture texture)
+	{
+		int handle = texture.getHandle();
+		if(handle == -1)
+		{
+			handle = generateTextureHandle();
+		}
+
+		if(handle != -1)
+		{
+			BitmapFactory.Options opts = new BitmapFactory.Options();
+			opts.inScaled = false;
+
+			Bitmap bmp = BitmapFactory.decodeResource(context.getResources(), texture.getDrawable(), opts);
+
+			GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureHandle);
+
+			GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+			GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+
+			GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+			GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+
+			GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bmp, 0);
+
+			texture.setWidth(bmp.getWidth());
+			texture.setHeight(bmp.getHeight());
+			texture.setHandle(handle);
+
+			bmp.recycle();
+		}
+	}
 }
