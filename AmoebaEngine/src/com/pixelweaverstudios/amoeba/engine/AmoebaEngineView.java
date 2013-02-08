f8007f37bf0d578faa661edf5a2e754917f15436
jsantangelo <jamesfsantangelo@gmail.com>
2013-02-07 21:43:41 -0500

    [#20] Added activity callbacks to View for future gameloop thread creation, added notes about other responsibilities of View.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/AmoebaEngineView.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/AmoebaEngineView.java
@@ -35,15 +35,36 @@ public class AmoebaEngineView extends GLSurfaceView
 	private void initializeRenderer()
 	{
 		//renderer = new GLES20Renderer(engine); - TODO (Mike)
+		//Renderer creation can be (should be?) separate from setting the renderer for this
+		//surface. setRenderer() should not be moved from this function though.
 		setRenderer(renderer);
 		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 	}
+
+	//TODO - need to determine when/where/who will register themselves with
+	//the SurfaceHolder as a SurfaceHolder.Callback, to do the following:
+	//* handle onSurfaceChanged (for screen dimension change handling, ie device rotation)
+	//* handle onSurfaceDestroyed (to stop the game loop thread)
 
 	//Methods from GLSurfaceView/SurfaceView/View
 	@Override
 	public boolean onTouchEvent(MotionEvent event)
 	{
 		gestureListener.onTouchEvent(event);
+	}
+
+	@Override
+	public void onPause()
+	{
+		setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		super.onPause();
+	}
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		//Start my thread here
 	}
 
 	// public void enableGesture(int motionEventType)
