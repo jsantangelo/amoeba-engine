64a8e9fc42e04f5f89b72a1dbe0b5ad9e6815d23
Mike Testen <mrtesten@gmail.com>
2013-02-10 19:15:20 -0500

    [#30] - Moved OpenGL calls from ShaderProgram to ShaderUtilities.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/shader/ShaderProgram.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/shader/ShaderProgram.java
@@ -2,8 +2,7 @@ package com.pixelweaverstudios.amoeba.graphics.shader;
 
 import java.util.ArrayList;
 
-import android.opengl.GLES20;
-import android.util.Log;
+import com.pixelweaverstudios.amoeba.graphics.utilities.ShaderUtilities;
 
 /**
  * @author Mike Testen
@@ -11,7 +10,6 @@ import android.util.Log;
  */
 public class ShaderProgram implements IShaderProgram
 {
-	private static final String TAG = "Program";
 	private ArrayList<IShader> shaders;
 	private int handle;
 
@@ -35,31 +33,15 @@ public class ShaderProgram implements IShaderProgram
 	 */
 	public int compileAndLink()
 	{
-		handle = GLES20.glCreateProgram();
-		if (handle == 0)
+		handle = ShaderUtilities.generateProgramHandle();
+		ShaderUtilities.attachShadersToProgram(handle, shaders);
+
+		if (!ShaderUtilities.linkProgram(handle))
 		{
-			throw new RuntimeException("Error creating program.");
+			ShaderUtilities.deleteProgram(handle);
+			handle = 0;
 		}
 
-		if (handle != 0)
-		{
-			for (int i = 0; i < shaders.size(); i++)
-			{
-				GLES20.glAttachShader(handle, shaders.get(i).getHandle());
-			}
-
-			GLES20.glLinkProgram(handle);
-
-			final int[] linkStatus = new int[1];
-			GLES20.glGetProgramiv(handle, GLES20.GL_LINK_STATUS, linkStatus, 0);
-
-			if (linkStatus[0] == 0)
-			{
-				Log.e(TAG, "Error compiling program: " + GLES20.glGetProgramInfoLog(handle));
-				GLES20.glDeleteProgram(handle);
-				handle = 0;
-			}
-		}
 		return handle;
 	}
 
@@ -70,7 +52,7 @@ public class ShaderProgram implements IShaderProgram
 	 */
 	public void use()
 	{
-		GLES20.glUseProgram(handle);
+		ShaderUtilities.useProgram(handle);
 	}
 
 	/**
@@ -80,10 +62,7 @@ public class ShaderProgram implements IShaderProgram
 	 */
 	public boolean isInUse()
 	{
-		final int[] currentProgram = new int[1];
-		GLES20.glGetIntegerv(GLES20.GL_CURRENT_PROGRAM, currentProgram, 0);
-
-		return (currentProgram[0] == handle);
+		return(ShaderUtilities.isProgramInUse(handle));
 	}
 
 	/**
@@ -93,7 +72,7 @@ public class ShaderProgram implements IShaderProgram
 	{
 		if (isInUse())
 		{
-			GLES20.glUseProgram(0);
+			ShaderUtilities.useProgram(0);
 		}
 	}
 
@@ -104,18 +83,7 @@ public class ShaderProgram implements IShaderProgram
 	 */
 	public int getAttributeLocation(final String attributeName)
 	{
-		if (attributeName == null)
-		{
-			throw new RuntimeException("attributeName is null");
-		}
-
-		int attribute = GLES20.glGetAttribLocation(handle, attributeName);
-		if (attribute == -1)
-		{
-			throw new RuntimeException("Attribute '" + attributeName + "' not found.");
-		}
-
-		return attribute;
+		return ShaderUtilities.getAttributeLocation(handle, attributeName);
 	}
 
 	/**
@@ -125,18 +93,7 @@ public class ShaderProgram implements IShaderProgram
 	 */
 	public int getUniformLocation(final String uniformName)
 	{
-		if (uniformName == null)
-		{
-			throw new RuntimeException("uniformName is null");
-		}
-
-		int uniform = GLES20.glGetUniformLocation(handle, uniformName);
-		if (uniform == -1)
-		{
-			throw new RuntimeException("Uniform '" + uniformName + "' not found.");
-		}
-
-		return uniform;
+		return ShaderUtilities.getUniformLocation(handle, uniformName);
 	}
 
 	/**
--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/utilities/ShaderUtilities.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/utilities/ShaderUtilities.java
@@ -1,5 +1,9 @@
 package com.pixelweaverstudios.amoeba.graphics.utilities;
 
+import java.util.ArrayList;
+
+import com.pixelweaverstudios.amoeba.graphics.shader.IShader;
+
 import android.opengl.GLES20;
 import android.util.Log;
 
@@ -40,5 +44,121 @@ public class ShaderUtilities
 			}
 		}
 		return handle;
+	}
+	
+	/**
+	 * @return
+	 */
+	public static int generateProgramHandle()
+	{
+		int handle = GLES20.glCreateProgram();
+		if (handle == 0)
+		{
+			throw new RuntimeException("Error creating program.");
+		}
+		
+		return handle;
+	}
+	
+	/**
+	 * @param programHandle
+	 * @param shaders
+	 */
+	public static void attachShadersToProgram(int programHandle, ArrayList<IShader> shaders)
+	{
+		for (int i = 0; i < shaders.size(); i++)
+		{
+			GLES20.glAttachShader(programHandle, shaders.get(i).getHandle());
+		}
+	}
+	
+	/**
+	 * @param handle
+	 * @return
+	 */
+	public static boolean linkProgram(int handle)
+	{
+		boolean result = true;
+		final int[] linkStatus = new int[1];
+		
+		GLES20.glLinkProgram(handle);
+		GLES20.glGetProgramiv(handle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+
+		if (linkStatus[0] == 0)
+		{
+			Log.e(TAG, "Error linking program: " + GLES20.glGetProgramInfoLog(handle));
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * @param programHandle
+	 */
+	public static void useProgram(int programHandle)
+	{
+		GLES20.glUseProgram(programHandle);
+	}
+	
+	/**
+	 * @param programHandle
+	 * @return
+	 */
+	public static boolean isProgramInUse(int programHandle)
+	{
+		final int[] currentProgram = new int[1];
+		GLES20.glGetIntegerv(GLES20.GL_CURRENT_PROGRAM, currentProgram, 0);
+
+		return (currentProgram[0] == programHandle);
+	}
+	
+	/**
+	 * @param programHandle
+	 */
+	public static void deleteProgram(int programHandle)
+	{
+		GLES20.glDeleteProgram(programHandle);
+	}
+	
+	/**
+	 * @param programHandle
+	 * @param attributeName
+	 * @return
+	 */
+	public static int getAttributeLocation(int programHandle, final String attributeName)
+	{
+		if (attributeName == null)
+		{
+			throw new RuntimeException("attributeName is null");
+		}
+
+		int attribute = GLES20.glGetAttribLocation(programHandle, attributeName);
+		if (attribute == -1)
+		{
+			throw new RuntimeException("Attribute '" + attributeName + "' not found.");
+		}
+
+		return attribute;
+	}
+	
+	/**
+	 * @param programHandle
+	 * @param uniformName
+	 * @return
+	 */
+	public static int getUniformLocation(int programHandle, final String uniformName)
+	{
+		if (uniformName == null)
+		{
+			throw new RuntimeException("uniformName is null");
+		}
+
+		int uniform = GLES20.glGetUniformLocation(programHandle, uniformName);
+		if (uniform == -1)
+		{
+			throw new RuntimeException("Uniform '" + uniformName + "' not found.");
+		}
+
+		return uniform;
 	}
 }
