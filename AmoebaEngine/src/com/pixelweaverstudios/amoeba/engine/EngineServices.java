1535a7315135eb4758667b570aa3d3edd4319371
jsantangelo <jamesfsantangelo@gmail.com>
2013-02-11 19:46:25 -0500

    [#24] Added initial version of engine services.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/EngineServices.java
@@ -0,0 +1,18 @@
+package com.pixelweaverstudios.amoeba.engine;
+
+public class EngineServices
+{
+	public EngineServices()
+	{
+		createDefaultComponents();
+	}
+
+	public void createDefaultComponents()
+	{	
+		//ScreenManager screenManager = new ScreenManager();
+		//InputManager inputManager = new InputManager();
+		//Renderer renderer = new Renderer();
+		//GameLoop gameLoop = new GameLoop();
+		//View view = new View();
+	}
+}
