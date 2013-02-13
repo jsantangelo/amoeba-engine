b406365ee329a524d00e9a4f08e77a2d863e1ebe
Mike Testen <mrtesten@gmail.com>
2013-02-12 20:40:32 -0500

    [#32] - Initial Shader Constants class.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/shader/ShaderConstants.java
@@ -0,0 +1,13 @@
+package com.pixelweaverstudios.amoeba.graphics.shader;
+
+public class ShaderConstants
+{
+	public static final String ATTRIBUTE_POSITION = "a_Position";
+	public static final String ATTRIBUTE_COLOR = "a_Color";
+	public static final String ATTRIBUTE_TEXTURECOORDINATES = "a_TextureCoordinates";
+
+	public static final String UNIFORM_MODELVIEWPROJECTIONMATRIX = "u_ModelViewProjectionMatrix";
+	public static final String UNIFORM_MODELVIEWMATRIX = "u_ModelViewMatrix";
+	public static final String UNIFORM_PROJECTIONMATRIX = "u_ProjectionMatrix";
+	public static final String UNIFORM_COLOR = "u_Color";
+}
