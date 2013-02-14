5642d14834257584aabfd16d643e65e62794cbc4
Mike Testen <mrtesten@gmail.com>
2013-02-13 21:36:13 -0500

    [#34] - Added TextureOptions class.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/texture/TextureOptions.java
@@ -0,0 +1,27 @@
+package com.pixelweaverstudios.amoeba.graphics.texture;
+
+import android.opengl.GLES20;
+
+public class TextureOptions
+{
+	public final int minFilter;
+	public final int magFilter;
+	public final float wrapS;
+	public final float wrapT;
+	
+	public TextureOptions(final int minFilter, final int magFilter, final int wrapS, final int wrapT)
+	{
+		this.minFilter = minFilter;
+		this.magFilter = magFilter;
+		this.wrapS = wrapS;
+		this.wrapT = wrapT;
+	}
+	
+	public void apply()
+	{
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, this.minFilter);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, this.magFilter);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S, this.wrapS);
+		GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T, this.wrapT);
+	}
+}
