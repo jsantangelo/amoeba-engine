1131297c34ea249fc207308567e041bb3a5c1c3d
Mike Testen <mrtesten@gmail.com>
2013-02-16 13:57:56 -0500

    [#34] - Added default texture options.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/texture/TextureOptions.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/texture/TextureOptions.java
@@ -4,6 +4,10 @@ import android.opengl.GLES20;
 
 public class TextureOptions implements ITextureOptions
 {
+	public static final TextureOptions CLAMP_NEAREST = new TextureOptions(GLES20.GL_NEAREST, GLES20.GL_NEAREST, GLES20.GL_CLAMP_TO_EDGE, GLES20.GL_CLAMP_TO_EDGE);
+	public static final TextureOptions CLAMP_LINEAR = new TextureOptions(GLES20.GL_LINEAR, GLES20.GL_LINEAR, GLES20.GL_CLAMP_TO_EDGE, GLES20.GL_CLAMP_TO_EDGE);
+	public static final TextureOptions DEFAULT = CLAMP_LINEAR;
+
 	public final int minFilter;
 	public final int magFilter;
 	public final float wrapS;
