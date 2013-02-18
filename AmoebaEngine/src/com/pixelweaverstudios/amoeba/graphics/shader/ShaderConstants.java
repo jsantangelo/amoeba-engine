a2a638bd6d817d324d343e9f5e42b93543b7ca88
Mike Testen <mrtesten@gmail.com>
2013-02-18 15:08:49 -0500

    [#32] - Updated shader contents with more variables.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/shader/ShaderConstants.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/graphics/shader/ShaderConstants.java
@@ -2,12 +2,16 @@ package com.pixelweaverstudios.amoeba.graphics.shader;
 
 public class ShaderConstants
 {
-	public static final String ATTRIBUTE_POSITION = "a_Position";
 	public static final String ATTRIBUTE_COLOR = "a_Color";
+	public static final String ATTRIBUTE_POSITION = "a_Position";
 	public static final String ATTRIBUTE_TEXTURECOORDINATES = "a_TextureCoordinates";
 
-	public static final String UNIFORM_MODELVIEWPROJECTIONMATRIX = "u_ModelViewProjectionMatrix";
+	public static final String UNIFORM_COLOR = "u_Color";
+	public static final String UNIFORM_MVPMATRIX = "u_MVPMatrix";
 	public static final String UNIFORM_MODELVIEWMATRIX = "u_ModelViewMatrix";
 	public static final String UNIFORM_PROJECTIONMATRIX = "u_ProjectionMatrix";
-	public static final String UNIFORM_COLOR = "u_Color";
+	
+	public static final String VARYING_COLOR = "v_Color";
+	public static final String VARYING_POSITION = "v_Position";
+	public static final String VARYING_TEXTURECOORDINATES = "v_TextureCoordinates";
 }
