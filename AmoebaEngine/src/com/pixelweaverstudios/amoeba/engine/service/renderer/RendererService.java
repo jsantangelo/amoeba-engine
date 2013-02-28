1a700886ffdeeaba0c6ed92d59e058750e020161
jsantangelo <jamesfsantangelo@gmail.com>
2013-02-27 20:59:07 -0500

    [#29] Renamed Renderer appropriately.

--- AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/service/renderer/RendererService.java
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/engine/service/renderer/RendererService.java
@@ -4,11 +4,9 @@ import android.opengl.GLSurfaceView;
 import javax.microedition.khronos.opengles.GL10;
 import javax.microedition.khronos.egl.EGLConfig;
 
-import com.pixelweaverstudios.amoeba.engine.IAmoebaEngine;
-
-public interface IAmoebaEngineRenderer extends GLSurfaceView.Renderer
+public interface RendererService extends GLSurfaceView.Renderer
 {
-	public void attachEngine(IAmoebaEngine engine);
+	public void attachEngine();
 	public void onDrawFrame(GL10 unused);
 	public void onSurfaceChanged(GL10 unused, int width, int height);
 	public void onSurfaceCreated(GL10 unused, EGLConfig config);
