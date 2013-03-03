d503ebc1c9dbdc48a51b3c852c39127b4ea69624
Mike Testen <mrtesten@gmail.com>
2013-03-03 15:40:44 -0500

    [#45] - Initial Entity interface.

---
+++ AmoebaEngine/src/com/pixelweaverstudios/amoeba/entity/Entity.java
@@ -0,0 +1,20 @@
+package com.pixelweaverstudios.amoeba.entity;
+
+public interface Entity
+{
+	public float getX();
+	public float getY();
+	public void setX(final float x);
+	public void setY(final float y);
+	
+	public float getRotation();
+	public void setRotation(final float rotation);
+	
+	public boolean isScaled();
+	public float getScale();
+	public float getScaleX();
+	public float getScaleY();
+	public void setScale(final float scale);
+	public void setScaleX(final float scaleX);
+	public void setScaleY(final float scaleY);
+}
