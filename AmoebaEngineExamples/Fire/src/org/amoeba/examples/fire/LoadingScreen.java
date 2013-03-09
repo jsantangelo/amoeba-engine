2c552f3f65493063cf6b270cd30b3814393d2388
jsantangelo <jamesfsantangelo@gmail.com>
2013-03-09 11:56:35 -0500

    [#55] Initial commit of test game.

---
+++ AmoebaEngineExamples/Fire/src/org/amoeba/examples/fire/LoadingScreen.java
@@ -0,0 +1,15 @@
+package org.amoeba.examples.fire;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoadingScreen extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
