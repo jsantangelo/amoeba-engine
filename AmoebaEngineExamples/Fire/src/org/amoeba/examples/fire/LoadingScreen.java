46c59c4d3c0039df078bd0ce1bf4aff6d12483db
Mike Testen <mrtesten@gmail.com>
2013-03-09 21:39:22 -0500

    [#55] Moved glClearColor to onDraw.

--- AmoebaEngineExamples/Fire/src/org/amoeba/examples/fire/LoadingScreen.java
+++ AmoebaEngineExamples/Fire/src/org/amoeba/examples/fire/LoadingScreen.java
@@ -2,15 +2,14 @@ package org.amoeba.examples.fire;
 
 import org.amoeba.activity.GameActivity;
 import org.amoeba.engine.routing.DrawListener;
-import org.amoeba.engine.routing.UpdateListener;
 import org.amoeba.engine.routing.InputListener;
+import org.amoeba.engine.routing.UpdateListener;
 import org.amoeba.engine.service.input.InputEvent;
 
-import android.util.Log;
-import android.app.Activity;
-import android.os.Bundle;
-import android.opengl.GLSurfaceView;
 import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.util.Log;
 
 public class LoadingScreen extends GameActivity
 	implements DrawListener, UpdateListener, InputListener
@@ -20,10 +19,10 @@ public class LoadingScreen extends GameActivity
 	private static int someNumber = 0;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-       
+
         registerForCallbacks();
 
         setContentView((GLSurfaceView) getView());
@@ -39,23 +38,7 @@ public class LoadingScreen extends GameActivity
     public void onDraw()
     {
     	Log.d(TAG, "onDraw handled");
-    }
 
-    public void onUpdate()
-    {
-    	Log.d(TAG, "onUpdate handled");
-    }
-
-    public void onInputEvent(InputEvent event)
-    {
-        Log.d(TAG, "onInputEvent handled");
-
-    	someNumber++;
-    	if (someNumber > 20)
-    	{
-    		someNumber = 0;
-    	}
-    	
     	if (someNumber < 10)
         {
             GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
@@ -64,5 +47,21 @@ public class LoadingScreen extends GameActivity
         {
             GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
         }
+    }
+
+    public void onUpdate()
+    {
+    	Log.d(TAG, "onUpdate handled");
+    }
+
+    public void onInputEvent(final InputEvent event)
+    {
+        Log.d(TAG, "onInputEvent handled");
+
+    	someNumber++;
+    	if (someNumber > 20)
+    	{
+    		someNumber = 0;
+    	}
     }
 }
