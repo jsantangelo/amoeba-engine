32355a7d819d856869523b0b43a6eef8621d276d
Mike Testen <mrtesten@gmail.com>
2013-03-12 21:11:56 -0400

    [#62] Fixed crashing issues.

--- AmoebaEngineExamples/SpriteExample/src/org/amoeba/examples/spriteexample/SpriteExample.java
+++ AmoebaEngineExamples/SpriteExample/src/org/amoeba/examples/spriteexample/SpriteExample.java
@@ -5,9 +5,6 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import org.amoeba.activity.GameActivity;
-import org.amoeba.engine.routing.DrawListener;
-import org.amoeba.engine.routing.InputListener;
-import org.amoeba.engine.routing.UpdateListener;
 import org.amoeba.engine.service.input.InputEvent;
 import org.amoeba.graphics.camera.Camera;
 import org.amoeba.graphics.shader.ShaderConstants;
@@ -20,8 +17,9 @@ import org.amoeba.graphics.utilities.TextureUtilities;
 
 import android.opengl.GLES20;
 import android.opengl.Matrix;
+import android.util.Log;
 
-public class SpriteExample extends GameActivity implements DrawListener, UpdateListener, InputListener
+public class SpriteExample extends GameActivity
 {
 	private float[] modelMatrix = new float[16];
 	private TextureShaderProgram program;
@@ -43,19 +41,37 @@ public class SpriteExample extends GameActivity implements DrawListener, UpdateL
 	private final int textureDataSize = 2;
 	private final int textureStrideBytes = textureDataSize * BYTES_PER_FLOAT;
 
+	private int screenWidth, screenHeight;
+
 	public SpriteExample()
 	{
+		screenWidth = 0;
+		screenHeight = 0;
+		final float[] positionData =
+		{
+			// X, Y, Z
+			-0.5f, 0.50f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			0.50f, 0.50f, 0.0f,
+			0.50f, -0.5f, 0.0f,
+		};
+		final float[] colorData =
+		{
+			// R, G, B, A
+			1.0f, 1.0f, 1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f, 1.0f,
+			1.0f, 1.0f, 1.0f, 1.0f
+		};
 
-		final float[] positionData = {
-				// X, Y, Z
-				-0.5f, 0.50f, 0.0f, -0.5f, -0.5f, 0.0f, 0.50f, 0.50f, 0.0f, 0.50f, -0.5f, 0.0f, };
-		final float[] colorData = {
-				// R, G, B, A
-				1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-
-		final float[] textureCoordData = {
-				// U, V
-				0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, };
+		final float[] textureCoordData =
+		{
+			// U, V
+			0.0f, 0.0f,
+			0.0f, 1.0f,
+			1.0f, 0.0f,
+			1.0f, 1.0f
+		};
 
 		positionBuffer = ByteBuffer.allocateDirect(positionData.length * BYTES_PER_FLOAT).order(ByteOrder.nativeOrder()).asFloatBuffer();
 		positionBuffer.put(positionData).position(0);
@@ -72,22 +88,29 @@ public class SpriteExample extends GameActivity implements DrawListener, UpdateL
 		texture = new BitmapTexture(textureUtilities, textureUtilities.getTextureOptionsPreset(Preset.DEFAULT), R.drawable.happy);
 	}
 
-	public void initialize()
+	@Override
+	public void onSurfaceCreated()
 	{
-		getEngine().registerForDraw(this);
-		getEngine().registerForUpdate(this);
-		getEngine().registerForInput(this);
-
+		Log.e("SpriteExample", "Compiling program.");
 		program.compile();
 		program.link();
 		texture.load();
 	}
 
+	@Override
+	public void onSurfaceChanged(final int width, final int height)
+	{
+		screenWidth = width;
+		screenHeight = height;
+	}
+
 	public void onDraw(final Camera camera)
 	{
+		Log.e("SpriteExample", "drawing");
 		program.use();
 		Matrix.setIdentityM(modelMatrix, 0);
-		Matrix.translateM(modelMatrix, 0, 250, 250, 0);
+		Matrix.translateM(modelMatrix, 0, screenWidth / 2, screenHeight /2, 0);
+        Matrix.scaleM(modelMatrix, 0, 100f, 100f, 1.0f);
 
 		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, texture.getHandle());
