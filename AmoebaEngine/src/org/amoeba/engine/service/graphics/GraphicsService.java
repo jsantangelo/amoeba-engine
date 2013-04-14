86860efd04f37a3cc70dabf3f8294c030b88196b
Mike Testen <mrtesten@gmail.com>
2013-04-14 19:03:11 -0400

    [#68] Created GraphicsService interface.

---
+++ AmoebaEngine/src/org/amoeba/engine/service/graphics/GraphicsService.java
@@ -0,0 +1,10 @@
+package org.amoeba.engine.service.graphics;
+
+/**
+ * Service provided by AmoebaEngine, responsible for preparing the
+ * graphics framework.
+ */
+public interface GraphicsService
+{
+
+}
