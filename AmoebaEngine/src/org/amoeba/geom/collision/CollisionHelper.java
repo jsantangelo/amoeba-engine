9fc5252aedd716508d72bfb4623a7ff579c4e514
Mike Testen <mrtesten@gmail.com>
2013-05-07 21:07:43 -0400

    [#79] Added an overloaded isColliding method for Rectangles.

--- AmoebaEngine/src/org/amoeba/geom/collision/CollisionHelper.java
+++ AmoebaEngine/src/org/amoeba/geom/collision/CollisionHelper.java
@@ -1,5 +1,7 @@
 package org.amoeba.geom.collision;
 
+import org.amoeba.geom.Rectangle;
+
 
 /**
  * CollisionHelper provides helper functions for collision between shapes.
@@ -13,6 +15,17 @@ public final class CollisionHelper
 	 * @return Whether the shapes are colliding.
 	 */
 	public static boolean isColliding(final Collidable shape1, final Collidable shape2)
+	{
+		return false;
+	}
+
+	/**
+	 * Determine whether a rectangle is colliding with another shape.
+	 * @param rect The rectangle.
+	 * @param shape The other shape.
+	 * @return Whether the shapes are colliding.
+	 */
+	public static boolean isColliding(final Rectangle rect, final Collidable shape)
 	{
 		return false;
 	}
