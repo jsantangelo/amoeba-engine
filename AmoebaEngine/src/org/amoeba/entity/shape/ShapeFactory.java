fae81183164d9a155ec2a0120cf7cee51c93ef6d
Mike Testen <mrtesten@gmail.com>
2013-05-09 19:48:50 -0400

    [#80] Created initial ShapeFactory.

---
+++ AmoebaEngine/src/org/amoeba/entity/shape/ShapeFactory.java
@@ -0,0 +1,26 @@
+package org.amoeba.entity.shape;
+
+import org.amoeba.geom.Dimension;
+import org.amoeba.geom.Point;
+
+/**
+ * ShapeFactory provides functionality to create shapes.
+ */
+public interface ShapeFactory
+{
+	/**
+	 * Create a 2d rectangle.
+	 * @param position The position (x, y) of the rectangle.
+	 * @param dimensions The dimensions (width, height) of the rectangle.
+	 * @return A 2d rectangle.
+	 */
+	public Rectangle2D createRectangle(final Point position, final Dimension dimensions);
+
+	/**
+	 * Create a 2d rectangle.
+	 * @param position The position (x, y) of the rectangle.
+	 * @param dimensions The dimensions (width, height) of the rectangle.
+	 * @return A 2d rectangle.
+	 */
+	public Rectangle2D createRoundedRectangle(final Point position, final Dimension dimensions);
+}
---
+++ AmoebaEngine/src/org/amoeba/entity/shape/impl/DrawableShapeFactory.java
@@ -0,0 +1,28 @@
+package org.amoeba.entity.shape.impl;
+
+import org.amoeba.entity.shape.Rectangle2D;
+import org.amoeba.entity.shape.ShapeFactory;
+import org.amoeba.geom.Dimension;
+import org.amoeba.geom.Point;
+
+/**
+ * DrawableShapeFactory provides functionality to create drawable shapes.
+ */
+public class DrawableShapeFactory implements ShapeFactory
+{
+
+	@Override
+	public Rectangle2D createRectangle(final Point position, final Dimension dimensions)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Rectangle2D createRoundedRectangle(final Point position, final Dimension dimensions)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
