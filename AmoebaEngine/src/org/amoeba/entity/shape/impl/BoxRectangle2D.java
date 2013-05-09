16a438d42d3c84236fb858e697c3f1f36100e0c0
Mike Testen <mrtesten@gmail.com>
2013-05-09 19:49:39 -0400

    [#84] Created BoxRectangle2D and RoundedRectangle2D.

---
+++ AmoebaEngine/src/org/amoeba/entity/shape/impl/BoxRectangle2D.java
@@ -0,0 +1,61 @@
+package org.amoeba.entity.shape.impl;
+
+import org.amoeba.entity.shape.Rectangle2D;
+import org.amoeba.graphics.camera.Camera;
+
+/**
+ * BoxRectangle2D is a 2D rectangle with square corners.
+ */
+public class BoxRectangle2D extends Rectangle2D
+{
+
+	@Override
+	public void onDraw(final Camera camera)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpdate()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColor(final int color)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColor(final float red, final float green, final float blue, final float alpha)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setColor(final int color, final long duration)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFill(final boolean filled)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
