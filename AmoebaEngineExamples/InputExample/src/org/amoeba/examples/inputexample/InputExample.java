393eb4e6a14919dc02ab97f844819bb557894c5b
jsantangelo <jamesfsantangelo@gmail.com>
2013-05-27 00:21:11 -0400

    [#96] Added scrolling list of event types as they happen to Input Example.

--- AmoebaEngineExamples/InputExample/src/org/amoeba/examples/inputexample/InputExample.java
+++ AmoebaEngineExamples/InputExample/src/org/amoeba/examples/inputexample/InputExample.java
@@ -1,15 +1,78 @@
 package org.amoeba.examples.inputexample;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.amoeba.activity.GameActivity;
+import org.amoeba.graphics.camera.Camera;
+import org.amoeba.engine.service.input.InputEvent;
 
-public class InputExample extends Activity
+import android.app.Activity;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
+public class InputExample extends GameActivity
 {
-    /** Called when the activity is first created. */
+	private static final String TAG = "AmoebaEngine.InputExample";
+
+    private TextView eventList = null;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
+    public void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        //Removing the Engine's GLSurfaceView's parent, so it can be reused in another layout.
+        ((ViewGroup) ((GLSurfaceView) getView()).getParent()).removeView((GLSurfaceView) getView());
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        relativeLayout.addView((GLSurfaceView) getView());
+        eventList = new TextView(this);
+        eventList.setMovementMethod(new ScrollingMovementMethod());
+        eventList.setWidth(120);
+
+        LayoutParams layoutParameters = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        layoutParameters.addRule(RelativeLayout.ALIGN_TOP);
+        eventList.setLayoutParams(layoutParameters);
+        eventList.setText("Touch the screen to see data on your inputs.\n\n");
+        eventList.setBackgroundColor(0x07000000);
+        relativeLayout.addView(eventList);
+
+        setContentView(relativeLayout);
     }
+
+    @Override
+    public void onDraw(final Camera camera)
+    {
+        GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1f);
+    }
+
+    @Override
+	public void onInputEvent(final InputEvent event)
+	{
+        eventList.append(event.getEventType().toString() + "\n");
+
+        eventList.post(new Runnable()
+        {
+            public void run()
+            {
+                final int scrollAmount = eventList.getLayout().getLineTop(eventList.getLineCount())
+                    - eventList.getHeight();
+
+                if (scrollAmount > 0)
+                {
+                    eventList.scrollTo(0, scrollAmount);
+                }
+                else
+                {
+                    eventList.scrollTo(0, 0);
+                }    
+            }
+        });
+	}
 }
