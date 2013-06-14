43fd0b824481e1c70e3bf914829bed8ef9296cf9
Mike Testen <mrtesten@gmail.com>
2013-06-13 20:56:51 -0400

    [#22] Added a texture implementation for text.

---
+++ AmoebaEngine/src/org/amoeba/graphics/texture/impl/TextTexture.java
@@ -0,0 +1,103 @@
+package org.amoeba.graphics.texture.impl;
+
+import org.amoeba.graphics.texture.Texture;
+import org.amoeba.graphics.texture.TextureOptions;
+
+/**
+ * TextTexture provides a texture with text.
+ */
+public class TextTexture implements Texture
+{
+
+	@Override
+	public void load()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unload()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isLoaded()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setWidth(final int width)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHeight(final int height)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setHandle(final int handle)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setID(final int id)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setOptions(final TextureOptions options)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getWidth()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHandle()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getID()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TextureOptions getOptions()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
