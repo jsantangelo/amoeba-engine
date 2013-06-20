9cc354232ab8c80ef139e13cf22692daa0d4f445
Mike Testen <mrtesten@gmail.com>
2013-06-20 19:51:47 -0400

    [#102] Removed Rectangle2D from the Sprite example.

--- AmoebaEngineExamples/SpriteExample/src/org/amoeba/examples/entity/SpriteExample.java
+++ AmoebaEngineExamples/SpriteExample/src/org/amoeba/examples/entity/SpriteExample.java
@@ -1,7 +1,6 @@
 package org.amoeba.examples.entity;
 
 import org.amoeba.activity.GameActivity;
-import org.amoeba.entity.shape.Rectangle2D;
 import org.amoeba.entity.sprite.Sprite;
 
 import android.graphics.Color;
@@ -11,15 +10,12 @@ import android.os.SystemClock;
 public class SpriteExample extends GameActivity
 {
 	private Sprite sprite;
-	private Rectangle2D rectangle;
-	private boolean flipped = false;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		sprite = getGraphicsService().getSpriteFactory().createSprite(R.drawable.happy);
-		rectangle = getGraphicsService().getShapeFactory().createRectangle(500f, 500f);
 	}
 
 	@Override
@@ -27,11 +23,6 @@ public class SpriteExample extends GameActivity
 	{
 		sprite.setPosition(width / 2, height * 3 /4);
 		sprite.setColor(Color.BLUE, 10000L);
-		sprite.setDepth(-1);
-
-		rectangle.setPosition(width / 2, height / 4);
-		rectangle.setColor(Color.RED, 10000L);
-		rectangle.setDepth(1);
 	}
 
 	@Override
@@ -41,18 +32,5 @@ public class SpriteExample extends GameActivity
 		float angleInDegrees = (360.0f / 10000.0f) * ((int) time);
 
 		sprite.setRotation(angleInDegrees);
-		rectangle.setRotation(angleInDegrees);
-
-		if (angleInDegrees > 355.0f && !flipped)
-		{
-			sprite.setDepth(sprite.getDepth() * -1);
-			rectangle.setDepth(rectangle.getDepth() * -1);
-			flipped = true;
-		}
-
-		if (angleInDegrees < 355.0f)
-		{
-			flipped = false;
-		}
 	}
 }
