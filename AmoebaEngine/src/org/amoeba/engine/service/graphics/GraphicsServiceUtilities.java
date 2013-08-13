5c8df0b3535e2ca661c244225c21da21e229bbd4
Mike Testen <mrtesten@gmail.com>
2013-08-13 14:14:15 -0400

    [#123] Added interface for graphics service utilities.

---
+++ AmoebaEngine/src/org/amoeba/engine/service/graphics/GraphicsServiceUtilities.java
@@ -0,0 +1,12 @@
+package org.amoeba.engine.service.graphics;
+
+import org.amoeba.engine.routing.SurfaceListener;
+
+/**
+ * GraphicsServiceUtilities provides processing during surface events
+ * in the graphics service.
+ */
+public interface GraphicsServiceUtilities extends SurfaceListener
+{
+
+}
