3bab6e92032cac58e880e63e6a5ec3a433254832
Mike Testen <mrtesten@gmail.com>
2013-08-14 14:13:25 -0400

    [#123] Added clear color functions to graphics service utilities interface.

--- AmoebaEngine/src/org/amoeba/engine/service/graphics/GraphicsServiceUtilities.java
+++ AmoebaEngine/src/org/amoeba/engine/service/graphics/GraphicsServiceUtilities.java
@@ -8,5 +8,18 @@ import org.amoeba.engine.routing.SurfaceListener;
  */
 public interface GraphicsServiceUtilities extends SurfaceListener
 {
+	/**
+	 * Set the GL Clear Color.
+	 * @param color The android.color to be used as the clear color.
+	 */
+	public void setClearColor(final int color);
 
+	/**
+	 * Set the GL Clear Color.
+	 * @param red The red component of the new clear color.
+	 * @param green The green component of the new clear color.
+	 * @param blue The blue component of the new clear color.
+	 * @param alpha The alpha component of the new clear color.
+	 */
+	public void setClearColor(final float red, final float green, final float blue, final float alpha);
 }
